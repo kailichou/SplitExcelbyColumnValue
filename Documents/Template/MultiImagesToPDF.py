a243fac9f4d7d681bb97e832baa85c3433fec805
kailichou <kailichou.edu@gmail.com>
2020-07-24 13:53:19 +0800

    kailichou

--- Documents/Template/MultiImagesToPDF.py
+++ Documents/Template/MultiImagesToPDF.py
@@ -1,61 +1,47 @@
 from PIL import Image
 import os
-# import tkinter as tk
 
-# HEIGHT = 300
-# WIDTH = 500
+def getName(filepath):
+    return os.path.basename(filepath)
 
+def getFile(filename):
+    return filename.split('/',1)[1]
 
-# root = tk.Tk()
-
-# canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-# canvas.pack()
-
-
-# frame = tk.Frame(root, bg='#80c1ff')
-# frame.place(relx=0.1, rely= 0.1, relwidth=0.8,relheight=0.8)
-
-# label = tk.Label(canvas, text='Do you want to transfer multiple images into one PDF file?')
-# label.pack()
-
-# yesButton = tk.Button(root, text = "YES", bg='lightgray')
-# yesButton.pack(side='center')
-
-# exitButton = tk.Button(root, text = " No, Exit the Program")
-# exitButton.pack(side='center')
-# noButton = tk.Button(root, text ='NO',bg='lightgrey')
-
-
-
-
-def getImage(filepath):
-
+def getImage(filepath, filename='/CombinedPDF.pdf'):
+    fname = getFile(filename)
     images = []
 
     for f in filepath:
         x = Image.open(f)
-        images.append(x)
+        if x.mode == 'RGBA':
+            x = x.convert('RGB')
+            images.append(x)
+        else:
+            images.append(x)
 
     im1 = images.pop(0)
     imagelist = images
-    if input(f"Would you like to save your file in {fileDirectory}? ====>").lower() == 'yes':
+    if input(f"Would you like to save your file in {fileDirectory}? ====>\n").lower() == 'yes':
         savepath = fileDirectory
     else:
-        print("Please specify the directory, you'd like to save your file\n")
+        print("Please specify the directory, you'd like to save your file.")
         savepath = input("===>")
+        if not os.path.exists(savepath):
+            print("Folder doesn't exist, we'll create a new folder", name,".")
+            os.makedirs(savepath)      
+
+        if savepath.rsplit('/',1)[-1] == '':
+            savepath = savepath.rsplit('/',1)[0]
         
-
-
-    im1.save(f'{savepath}/Combined.pdf',save_all=True, append_images=imagelist)
-    print(f'Thank you for using this program. The file is saved at {savepath}')
-
+    
+    name = getName(savepath)
+    im1.save(savepath +'/'+ fname, save_all=True, append_images=imagelist)
+    print(f"Thank you for using this program. \nThe file {fname} is saved at folder {name}.")
             
 
 
 fileDirectory = os.getcwd()
 
-filepaths = input("Please add images that you want to convert into a PDF file and seperate them with a comma ','===> \n").split(',')
-getImage(filepaths)
+filepaths = input("Please add the pathname of images that you want to convert into a PDF file and seperate them with a comma ','\n===>").split(',')
+getImage(filepaths,filename='/CombinedPDF.pdf')
 
-
-#root.mainloop()
